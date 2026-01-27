79c122c04ab6f28954a21994d735f4c3c58ec5d9
Anjana Edwin <anjanaedwin@gmail.com>
2026-01-27 23:56:00 +0000

    Resume Feedback page

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,4 +1,6 @@
-import {useParams} from "react-router";
+import {Link, useNavigate, useParams} from "react-router";
+import {useEffect, useState} from "react";
+import {usePuterStore} from "~/lib/puter";
 
 
 export const meta = () => ([
@@ -7,11 +9,54 @@ export const meta = () => ([
 ])
 
 const Resume = () => {
+    const{auth, isLoading, fs, kv} = usePuterStore();
     const {id} = useParams();
+    const [imageUrl,setImageUrl] =useState('');
+    const[resumeUrl,setResumeUrl]=useState('');
+    const [feedbackUrl,setFeedbackUrl]=useState('');
+    const navigate=useNavigate();
+
+    useEffect(() => {
+        const loadResume = async () =>{
+            const resume = await kv.get(`/resume/${id}`);
+
+            if(!resume) return;
+
+            const data =JSON.parse(resume);
+
+            const resumeBlob = await fs.read(data.resumePath);
+            if(!resumeBlob) return;
+
+            const pdfBlob = new Blob([resumeBlob], { type: 'application/pdf' });
+            const resumeUrl = URL.createObjectURL(pdfBlob);
+            setResumeUrl(resumeUrl);
+
+            const imageBlob =await fs.read(data.imagePath)
+            if(!imageBlob) return;
+            const imageUrl = URL.createObjectURL(imageBlob);
+            setImageUrl(imageUrl);
+            setFeedbackUrl(data.feedback);
+        }
+        loadResume();
+    },[id]);
 
     return (
-     <main>
+     <main className="!pt-0">
+         <nav className="resume-nav">
+           <Link to="/" className="back-button">
+               <img src="/icons/back.svg" alt="logo" className="w-2.5 h-2.5" />
+               <span className="text-gray-800 text-sm font-semibold">Back to Home Page</span>
+           </Link>
+         </nav>
+        <div className="flex flex-row w-full max-lg:flex-col-reverse">
+            <section className="feedback-section ">
+                {imageUrl && resumeUrl && (
+                    <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
 
+                    </div>
+                )}
+            </section>
+        </div>
      </main>
     )
 }
