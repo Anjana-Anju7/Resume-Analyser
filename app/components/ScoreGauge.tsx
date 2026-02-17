723d5fb30ba2dae1b83a90c7d9d421304fb638d1
Anjana Edwin <anjanaedwin@gmail.com>
2026-02-17 22:00:10 +0000

    vercel config files

--- app/components/ScoreGauge.tsx
+++ app/components/ScoreGauge.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+  import { useEffect, useRef, useState } from "react";
 
 const ScoreGauge = ({ score = 75 }: { score: number }) => {
     const [pathLength, setPathLength] = useState(0);
